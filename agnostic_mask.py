5b8b549ad647974be0b948c8b1e905f26e58948a
miniwa00 <miniwa00@gmail.com>
2024-08-27 22:36:42 +0900

    presinged_url

--- agnostic_mask.py
+++ agnostic_mask.py
@@ -1,16 +1,13 @@
 import os
 import time
 from huggingface_hub import snapshot_download
+import requests
 from model.cloth_masker import AutoMasker
 import json
 import base64
 from PIL import Image
 from io import BytesIO
 import boto3
-
-# from dotenv import load_dotenv
-
-# load_dotenv()
 
 AWS_ACCESS_KEY_ID = str(os.environ["ACCESS_KEY_ID"])
 AWS_SECRET_ACCESS_KEY = str(os.environ["SECRET_ACCESS_KEY"])
@@ -20,6 +17,7 @@ s3 = boto3.client(
     region_name="ap-northeast-2",
     aws_access_key_id=AWS_ACCESS_KEY_ID,
     aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    endpoint_url="https://s3.ap-northeast-2.amazonaws.com/",
 )
 
 
@@ -28,13 +26,12 @@ def save_and_upload_s3(mask, username, cloth_type, start_timestamp):
     output_dir = "/tmp/mask_results"
     os.makedirs(output_dir, exist_ok=True)
 
-    # timestamp = time.strftime("%Y%m%d-%H%M%S", time.localtime())
     new_image_name = f"{username}_{start_timestamp}_{cloth_type}.png"
     mask.save(os.path.join(output_dir, new_image_name))
 
     filename = os.path.join(output_dir, new_image_name)
     bucket_name = "githubsalt-bucket"
-    object_name = f"{username}/masking/{start_timestamp}/{cloth_type}.png"
+    object_name = f"users/{username}/masking/{start_timestamp}/{cloth_type}.png"
 
     s3.upload_file(filename, bucket_name, object_name)
 
@@ -49,9 +46,6 @@ def get_mask(image, cloth_type, username, start_timestamp):
         device="cpu",
     )
 
-    image = base64.b64decode(image)
-    image = Image.open(BytesIO(image))
-
     mask = automasker(image, cloth_type)["mask"]
 
     save_and_upload_s3(
@@ -64,19 +58,24 @@ def get_mask(image, cloth_type, username, start_timestamp):
 
 def handler(event, context):
     start = time.time()
-    start_timestamp = time.strftime("%Y%m%d-%H%M%S", time.localtime())
+    # start_timestamp = time.strftime("%Y%m%d-%H%M%S", time.localtime())
 
     body = event["body-json"]
 
     username = body["username"]
-    img = body["img"]
+    row_image_url = body["row_image_url"]
+    timestamp = body["timestamp"]
+
+    response = requests.get(row_image_url)
+    if response.status_code == 200:
+        row_image = Image.open(BytesIO(response.content))
 
     for cloth_type in ["upper", "lower", "overall", "inner", "outer"]:
         get_mask(
-            image=img,
+            image=row_image,
             cloth_type=cloth_type,
             username=username,
-            start_timestamp=start_timestamp,
+            start_timestamp=timestamp,
         )
 
     result = f"Hello, {username}"
@@ -88,6 +87,7 @@ def handler(event, context):
 
 if __name__ == "__main__":
     start = time.time()
+    start_timestamp = time.strftime("%Y%m%d-%H%M%S", time.localtime())
 
     with open(
         "C:/Users/user/OneDrive/documents/Dev/new_cat/test_dataset/test/image/jacket.jpg",
@@ -98,6 +98,6 @@ if __name__ == "__main__":
     img = encoded_image
 
     for cloth_type in ["upper", "lower", "overall", "inner", "outer"]:
-        get_mask(img, cloth_type, username="test")
+        get_mask(img, cloth_type, username="test", start_timestamp=start_timestamp)
 
     print("Time taken: ", time.time() - start)
